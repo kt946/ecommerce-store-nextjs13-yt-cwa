34e150862ee98ae20795252ce8f6c57b93a26699
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-09-30 00:08:16 -0400

    Added Billboard to homepage

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,19 @@
-const HomePage = () => {
-  return <div>Hello Store</div>;
+import getBillboard from '@/actions/get-billboard';
+import Billboard from '@/components/billboard';
+import Container from '@/components/ui/container';
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("03730391-ac4b-4d97-bde5-35e0ec2cd361");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
