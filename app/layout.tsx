c391dea5b43e012ae0f9e818cf21ce83a470ab64
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-09-29 23:08:37 -0400

    Added Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,24 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Urbanist } from 'next/font/google'
+import type { Metadata } from 'next';
+import { Urbanist } from 'next/font/google';
 
-const font = Urbanist({ subsets: ['latin'] })
+import Footer from '@/components/footer';
+
+import './globals.css';
+
+const font = Urbanist({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Store',
   description: 'Store',
-}
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        {children}
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
