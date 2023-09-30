f3c9e727323a553939fe4aa36cbeaa8dcbea11e5
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-09-30 13:48:08 -0400

    Added toast and use-cart hook

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Urbanist } from 'next/font/google';
 import Footer from '@/components/footer';
 import Navbar from '@/components/navbar';
 import ModalProvider from '@/providers/modal-provider';
+import ToastProvider from '@/providers/toast-provider';
 
 import './globals.css';
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToastProvider />
         <ModalProvider />
         <Navbar />
         {children}
