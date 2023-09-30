691641d65f24f22e8cc3b6bd5dfca0246157e876
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-09-30 13:39:47 -0400

    Added Preview Modal component, provider, and hook

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Urbanist } from 'next/font/google';
 
 import Footer from '@/components/footer';
 import Navbar from '@/components/navbar';
+import ModalProvider from '@/providers/modal-provider';
 
 import './globals.css';
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={font.className}>
+        <ModalProvider />
         <Navbar />
         {children}
         <Footer />
